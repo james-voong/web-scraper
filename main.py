555b4cd1b7a57e05f68808803208fcf490402902
James Voong <James.Voong.91@gmail.com>
2021-07-23 00:24:55 +0100

    :bug: Needed to add arial-unicode-ms-bold for use when outputting a bold title.

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ end_chapter = 1649
 start_cut = 2
 end_cut = 26
 
+test = False
+
 for chapter_num in range(start_chapter, end_chapter):
     response = requests.get(f'{url}{chapter_num}/')
     soup = BeautifulSoup(response.text, "html.parser")
@@ -20,6 +22,10 @@ for chapter_num in range(start_chapter, end_chapter):
     text_elements = [t for t in soup.find_all(text=True) if t.parent.name in whitelist]
     endpoint = len(text_elements) - end_cut
 
+    if test:
+        for index, value in enumerate(text_elements):
+            print(f'[{index}] {value}')
+
     text_elements = text_elements[start_cut:endpoint]
     chapter_name = text_elements[0].replace(':', ' -')
 
@@ -27,11 +33,12 @@ for chapter_num in range(start_chapter, end_chapter):
     pdf.add_page()
 
     pdf.add_font('ArialUnicode', '', 'arial-unicode-ms.ttf', uni=True)
+    pdf.add_font('ArialUnicode', 'B', 'arial-unicode-ms-bold.ttf', uni=True)
     pdf.set_font('ArialUnicode', '', 11)
 
     for index, value in enumerate(text_elements):
         if index == 0:
-            pdf.set_font('Arial', 'B', 13)
+            pdf.set_font('ArialUnicode', 'B', 13)
             pdf.write(5, value+'\n\n')
             pdf.set_font('ArialUnicode', '', 11)
             continue
